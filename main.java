02daa7faa8523470f4e36073f0f3534117fdfa21
solomongo™ <80227002+solomongoGUI@users.noreply.github.com>
2022-10-09 14:37:27 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,47 @@
+/*
+ 1.- Realiza una clase finanzas que convierta dólares a euros y viceversa. Como atributo tiene un número decimal para almacenar a como está el cambio entre ambas monedas.
+
+Como constructores tiene:
+
+     Un constructor finanzas() por defecto, el cual establecerá el cambio Dolares a Euros en 1.06 (1 dólar son 1.06 euros).
+     Un constructor finanzas(double), el cual permitirá configurar el cambio dólar a euro.
+
+Codifica los métodos dolaresToEuros y eurosToDolares. Ambos métodos reciben la cantidad de dinero a cambiar, realizan la conversión de la moneda y devuelven la respuesta. 
+
+Prueba que dicha clase funciona correctamente haciendo conversiones entre euros y 
+dólares realizando las siguientes acciones:
+
+     Crea un objeto finanzas denominado finanza con el constructor por defecto.
+     Pide al usuario que te introduzca los dolares que tiene y muestra cuantos euros son.
+     Pide al usuario que te introduzca los euros que tiene y muestra cuantos dolares son.
+     Crea otro objeto finanza2 con el cambio a 1.10 y con los dolares introducidos anteriormente por el usuario muestra cuantos euros son.
+*/
+
+package com.solomongo.ejercicio1;
+import java.util.Scanner;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+      Scanner teclado1 = new Scanner(System.in); 
+      float cantidad; 
+      System.out.print("\n\n"); 
+      System.out.print("Introduce una cantidad de euros: "); 
+      cantidad=teclado1.nextFloat(); System.out.print(cantidad);
+      System.out.printf("euros (al cambio 1.06)= %,.2f",(cantidad*1.06));
+      System.out.print("$.\n\n+++++++++++++++++++++++++++++++++++++");
+
+ Finanzas opera=new Finanzas(); // sin paramts opera() llegaran del SET. Es new, otra clase 
+ opera.setDolarAEuro(cantidad*1.06); // con param(cantidad....) se asignan aqui.
+ opera.setEuroADolar(cantidad/1.06);
+ //System.out.printf("Euros a Dolares: %,.2f",opera.getEuroADolar());  
+       Scanner teclado2 = new Scanner(System.in);
+       System.out.println("\n\nIntroduce una cantidad de dolares: "); 
+       float cantidad2 = teclado2.nextFloat(); System.out.print(cantidad);
+       System.out.printf("$ (al cambio 1.06) = %,.2f",(cantidad2/1.06));
+       System.out.print("euros.\n");   
+ System.out.print(cantidad);      
+ System.out.printf("$ (al cambio 1.10) = %,.2f",(cantidad2/1.10));
+ System.out.print("euros.\n\n+++++++++++++++++++++++++++++++++++++");
+ //System.out.printf("Dolares a Euros: %,.2f",opera.getDolarAEuro());  
+ System.out.print("+++++++++++++++++++++++++++++++++++++\n\n");
+    }
+}
